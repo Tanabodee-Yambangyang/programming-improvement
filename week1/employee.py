43107b638ba1c0851a6d02eb17f61834690bc9af
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2022-09-12 00:45:04 +0700

    Finished: Small programming practice on OOP.

---
+++ week1/employee.py
@@ -0,0 +1,8 @@
+# this goes in employee.py
+
+def employee_id():
+    print("Employee Unique Identity!")
+
+
+# just a variable
+Age = "Employee age is **"
---
+++ week1/employee_cls.py
@@ -0,0 +1,12 @@
+
+
+class Employee:
+    """A class for collecting data for each employee."""
+
+    def __init__(self):
+        """Initialize Employee object."""
+        self. age = "Employee Age is ##"
+
+    def get_id(self):
+        """Print ID of employee."""
+        print("This is just employee unique identity")
